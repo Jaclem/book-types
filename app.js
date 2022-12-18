3cf007ce72aac182f4c161b947fb6d77deef12b1
Joshua Clement <jaclem12@gmail.com>
2022-12-18 10:29:41 -0500

    Added some styling and refactored addBookToLibrary function

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const table = document.getElementById('table');
 const addNew = document.getElementById('add-new');
 const addBtn = document.getElementById('add-btn');
-const exitBtn = document.getElementById('exit-btn');
+const exitBtn = document.getElementById('close');
 const newEntry = document.getElementById('new-entry');
 const titleInput = document.getElementById('title');
 const authorInput = document.getElementById('author');
 const pageInput = document.getElementById('pages');
 const readInput = document.getElementById('read');
-
+const modalBackground = document.getElementById('modal-background');
 
 let myLibrary = [];
 let tableRow;
@@ -20,47 +20,49 @@ function Book(title, author, pages, read){
     this.read = read
 }
 
+Book.prototype.changeReadStatus = function() {
+    console.log(`${this.read}`);
+}
+
+
+
 // then checks to see if inputs are blank and lets user know they are required if true
 // finally adds myLibrary array items to the page
-function addBookToLibrary(title, author, pages, read) {
+function addBookToLibrary() {
+    let bookObj = Object.values(Book);
 
-    if(title == '' || author == '' || pages == ''){
+    if(Book.title == '' || Book.author == '' || Book.pages == ''){
         const required = document.getElementById('required');
         required.innerText = "All fields are required";
         required.style.color = "red";
     }
     else {
         newEntry.classList.remove('visibility');
+        tableRow = document.createElement('tr');
+        table.appendChild(tableRow);
 
-        for(let i = 0; i < myLibrary.length; i++){
-            tableRow = document.createElement('tr');
-            table.appendChild(tableRow); 
-        };
-
-        const bookArr = [`${title}`, `${author}`, `${pages}`, `${read}`];
-        
-        bookArr.forEach(list => {
+        for(let i = 0; i < bookObj.length; i++){
             tableData = document.createElement('td');
-            tableData.innerText = list;
+            tableData.innerText = bookObj[i];
             tableRow.appendChild(tableData);
-        });
-
+        }
         createTrashBtn();
     }
 }
 
 // creates a new book object and adds it to myLibrary array
 function createBook() {
-    const books = Object.create(Book);
+    // const books = Object.create(Book);
 
-    books.title = titleInput.value;
-    books.author = authorInput.value;
-    books.pages = pageInput.value;
-    books.read = readInput.value;
+    Book.title = titleInput.value;
+    Book.author = authorInput.value;
+    Book.pages = pageInput.value;
+    Book.read = readInput.value;
 
-    myLibrary.push(books);
+    myLibrary.push(Book);
+    addBookToLibrary();
 
-    addBookToLibrary(books.title, books.author, books.pages, books.read);
+    // addBookToLibrary(books.title, books.author, books.pages, books.read);
 }
 
 function createTrashBtn() {
@@ -83,6 +85,8 @@ function createTrashBtn() {
 }
 
 
+
+
 // Event Listeners
 
 addNew.addEventListener('click', (e)=> {
@@ -93,7 +97,6 @@ addNew.addEventListener('click', (e)=> {
 addBtn.addEventListener('click', (e)=> {
     e.preventDefault();
     createBook();
-    // addBookToLibrary();
 });
 
 exitBtn.addEventListener('click', (e) => {
